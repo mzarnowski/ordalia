415e0fee809708913689ab5645aaa5ab69efc19f
mzarnowski <marek1840+github@gmail.com>
2023-05-29 13:31:42 +0200

    Initialize Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    application
+}
+
+group = "dev.mzarnowski.cinema"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.assertj:assertj-core:3.23.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
